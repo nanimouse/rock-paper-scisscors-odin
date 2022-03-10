dd0c1bfdcc5ce0d307108ae2732c065ea8847bd9
Nikki <0120codes@gmail.com>
2022-03-09 20:41:22 -0500

    completing Rock Paper Scissors
    i cannot believe i just completed this!
    
    - added variables to take score, and incremented them within the playRound() function.
    - created game() function with a for loop and an if statement. it plays 5 rounds of the game and tells the final winner with score.
    
    it felt like much more than two bullets lol
    
    my patience was tested but so were my problem solving skills. i learned the importance of taking breaks, because this would NOT have gotten done if i tried to go straight thru.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const arr = ["Rock", "Paper", "Scissors"]; //create an array that holds the cpu commands
+const arr = ["Rock", "Paper", "Scissors"]; //create an array that holds the cpu choices
+
+let playerScore = 0;
+let computerScore = 0;
+// score taking
+
+
 
 function computerPlay() {
    let compChoice = arr[Math.floor( Math.random() * arr.length )]; 
      // since .random gives a # b/t 0-1 [a decimal], we use .floor to give us a whole number (it rounds down), then we multiply by the array length so that we get a result b/t 0-length of array
    let opponent = compChoice.toLowerCase();
    return opponent;
-    //we assigned our randomization calculation to "opponent" so here we are instructing the program to return the random value
+    //we assigned our randomization calculation to "opponent" so here we are instructing the program to return the random value, but we couldve also return compChoice for less code
 }
-//function that will return the choice (random) from the cpu
+//function that will return the choice (random) from the cpu in lowercase
 
 
 function playRound(playerSelection, computerSelection) {
@@ -17,8 +23,10 @@ function playRound(playerSelection, computerSelection) {
         if (playerSelection.toLowerCase() === "rock"){
             return "rock and roll, its a tie!";
         } else if (playerSelection.toLowerCase() === "paper"){
+            playerScore++;                                          //increments playerScore
             return "You win, paper beats rock!";
         } else if (playerSelection.toLowerCase() === "scissors"){
+            computerScore++;                                        //increments computerScore
             return "You lose, rock beats scissors!";
         } else {
             return "Please enter rock, paper, or scissors!";
@@ -26,10 +34,12 @@ function playRound(playerSelection, computerSelection) {
 
     } else if (computerSelection === "paper"){
         if (playerSelection.toLowerCase() === "rock"){
+            computerScore++;
             return "You lose, paper beats rock!";
         } else if (playerSelection.toLowerCase() === "paper"){
             return "Paper on paper, its a tie!";
         } else if (playerSelection.toLowerCase() === "scissors"){
+            playerScore++;
             return "You win, scissors cut paper!";
         } else {
             return "Please enter rock, paper, or scissors!";
@@ -37,24 +47,48 @@ function playRound(playerSelection, computerSelection) {
 
     } else if (computerSelection === "scissors"){
         if (playerSelection.toLowerCase() === "rock"){
+            playerScore++;
             return "You win, rock beats scissors!";
         } else if (playerSelection.toLowerCase() === "paper"){
+            computerScore++;
             return "You lose, scissors cut paper!";
         } else if (playerSelection.toLowerCase() === "scissors"){
             return "Saved by the tie!";
         } else {
             return "Please enter rock, paper, or scissors!";
         }
+    }
+}
+// function that plays one round
+
+
+
+function game(){
+
+    for (i = 1; i < 6; i++){
+
+        const playerSelection = prompt("Rock Paper Scissors says Shoot!"); //gets input value
+        const computerSelection = computerPlay(); //stores computer choice
+
+        console.log("Round " + i);
+        console.log(playerSelection);
+        console.log(computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
 
     }
-    // testing if cpu chooses rock with child if statements (it works!)
 
+    if (playerScore > computerScore){
+        console.log("HOORAYYY! YOU WON THE GAME! " + playerScore + " to " + computerScore);
+    } else if (playerScore < computerScore){
+        console.log("Better luck next time! " + playerScore + " - " + computerScore);
+    } else {
+        console.log("Tied game! " + playerScore + " - " + computerScore);
+    } 
+}
+// plays 5 rounds and returns the winner of each round, then the final score!
 
+// WE HAVE IT ASKING 5 TIMES BUT NOW WE NEED THE COMP TO GIVE 5 DIFFERENT ANSWERS
+// fixed by adding computerSelection to loop
 
-
-const playerSelection = "paper";
-const computerSelection = computerPlay();
-
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+game();
+// plays Rock Paper Scissors!
